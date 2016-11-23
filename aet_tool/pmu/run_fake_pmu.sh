63947d2471a6a3c2484e1d694ff39db18d3a0201
houfang <houfangcq@gmail.com>
2016-11-23 17:30:20 +0800

    push two experiment result image

--- aet_tool/pmu/run_fake_pmu.sh
+++ aet_tool/pmu/run_fake_pmu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 tmp_file=tmp.txt
-#xl dm -c >> /dev/null
+xl dm -c >> /dev/null
 array=$1
 round=$2
 domu_cpu=1
@@ -11,8 +11,8 @@ xl vcpu-pin 0 0 $dom0_cpu
 xl vcpu-pin 1 0 $domu_cpu
 XC_DIR=../control
 PMU_DIR=./
-./a.out 2 1 1 0 # open monitor
-#xl dm -c
+./a.out 2 1 1 2 # open monitor
+xl dm -c
 echo "------------------------"
 $XC_DIR/xc -r
 $XC_DIR/xc -s 0 > tmp.txt
@@ -32,9 +32,9 @@ $XC_DIR/xc -s 1 > tmp.txt
 cat tmp.txt
 PF2=`cat tmp.txt | grep "page fault" | awk -F ':' '{print $2}'`
 echo "------------------------"
-#xl dm -c | tail -n 16 
+xl dm -c | tail -n 16 
 ./a.out 2 1 0 0 #close monitor
-#xl dm -c
+xl dm -c
 echo "------------------------"
 echo "$PF2 - $PF1"
 PFN=$(($PF2-$PF1))
